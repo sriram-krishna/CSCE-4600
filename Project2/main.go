afd361f6058c45ae315b8fb6743b7ef437e25690
Sriram Krishna Kumar <sriramkrishnakumar@my.unt.edu>
2024-02-21 14:34:17 -0600

    changed repo

--- Project2/main.go
+++ Project2/main.go
@@ -9,7 +9,7 @@ import (
 	"os/user"
 	"strings"
 
-	"github.com/jh125486/CSCE4600/Project2/builtins"
+	"github.com/sriram-krishna/CSCE-4600/Project2/builtins"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/jh125486/CSCE4600
+module github.com/sriram-krishna/CSCE-4600
 
 go 1.19
 
